4679552e0d29a79fa09965594b40dc9c75d480fe
[mr.brain1201] <[mr.brain1201@gmail.com]>
2021-04-26 19:32:16 +0300

    lesson_7 inception_1

--- instagram_parse/settings.py
+++ instagram_parse/settings.py
@@ -14,7 +14,8 @@ NEWSPIDER_MODULE = 'instagram_parse.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'instagram_parse (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/90.0.4430.72 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -25,22 +26,22 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.05
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -62,22 +63,22 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'instagram_parse.pipelines.InstagramParsePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'instagram_parse.pipelines.InstagramParsePipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- instagram_parse/spiders/instagram.py
+++ instagram_parse/spiders/instagram.py
@@ -1,10 +1,41 @@
+import json
+
 import scrapy
 
 
 class InstagramSpider(scrapy.Spider):
-    name = 'instagram'
-    allowed_domains = ['www.instagram.com']
-    start_urls = ['http://www.instagram.com/']
+    name = "instagram"
+    allowed_domains = ["www.instagram.com"]
+    start_urls = ["https://www.instagram.com/"]
+    _login_url = "/accounts/login/ajax/"
+    _tags_path = "/explore/tags/"
 
-    def parse(self, response):
-        pass
+    def __init__(self, login, password, tags, *args, **kwargs):
+        self.login = login
+        self.password = password
+        self.tags = tags
+        super().__init__(*args, **kwargs)
+
+    def parse(self, response, **kwargs):
+        try:
+            js_data = self.js_data_extract(response)
+            yield scrapy.FormRequest(
+                response.urljoin(self._login_url),
+                method="POST",
+                callback=self.parse,
+                formdata={"username": self.login, "enc_password": self.password,},
+                headers={"X-CSRFToken": js_data["config"]["csrf_token"]},
+            )
+        except AttributeError:
+            if response.json()["authenticated"]:
+                for tag in self.tags:
+                    yield response.follow(f"{self._tags_path}{tag}/", callback=self.tag_page_parse)
+
+    def tag_page_parse(self, response):
+        print(1)
+
+    def js_data_extract(self, response):
+        script = response.xpath(
+            "//body/script[contains(text(), 'window._sharedData =')]/text()"
+        ).extract_first()
+        return json.loads(script.replace("window._sharedData = ", "")[:-1])
